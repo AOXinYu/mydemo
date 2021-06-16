df3dcdf40d14731c851814c1e522d0963c31b107
aoxy@raiyee.com <aoxy@raiyee.com>
2021-06-16 11:13:13 +0800

    Initial commit

---
+++ src/main/java/com/example/mydemo/controller/helloWorldContoller.java
@@ -0,0 +1,15 @@
+package com.example.mydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloWorldContoller {
+    @ResponseBody
+    @RequestMapping("/")
+    public String hello(){
+        return "hello SpringBoot!";
+    }
+
+}
