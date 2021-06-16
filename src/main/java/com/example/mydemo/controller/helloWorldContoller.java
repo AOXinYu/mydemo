e73a74e57ecacfa81d42983a63b3a05ae6478d12
aoxy@raiyee.com <aoxy@raiyee.com>
2021-06-16 15:36:26 +0800

    第四次测试

--- src/main/java/com/example/mydemo/controller/helloWorldContoller.java
+++ src/main/java/com/example/mydemo/controller/helloWorldContoller.java
@@ -9,7 +9,7 @@ public class helloWorldContoller {
     @ResponseBody
     @RequestMapping("/")
     public String hello(){
-        return "测试，完成了GitHub设置并且配置好Jenkins ，第三次测试";
+        return "测试，完成了GitHub设置并且配置好Jenkins ，第四次测试";
     }
 
 }
